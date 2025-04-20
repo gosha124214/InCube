d87118bfc21e48dff05cb2217c1588bee7cc99da
gosha124214 <gosha.lih@gmail.com>
2025-04-20 18:18:24 +0300

    21.04.2025V2

--- AppInCube/AppInCube/obj/Debug/net8.0-ios/ios-arm64/AppInCube.AssemblyInfo.cs
+++ AppInCube/AppInCube/obj/Debug/net8.0-ios/ios-arm64/AppInCube.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppInCube")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+04df42a67263988ef30046afd22559d3ce834326")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppInCube")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppInCube")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
